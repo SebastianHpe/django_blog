21d23767536801ee2a07e170e8be1e35e0019983
SebastianHpe <sebastianhoppe@hotmail.de>
2025-07-28 18:53:57 +0200

    feat: Add dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        "author": "Sib",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "July 27,  2025"
+    },
+    {
+        "author": "Lisle",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "July 28,  2025"
+    }
+]
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "About"})
