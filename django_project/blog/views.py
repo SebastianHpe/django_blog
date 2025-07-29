3fc30a4d8bae4ee81c8ca71a16061b475adbbac1
SebastianHpe <sebastianhoppe@hotmail.de>
2025-07-29 12:12:39 +0200

    feat: Display posts from database instead of dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        "author": "Sib",
-        "title": "Blog post 1",
-        "content": "First post content",
-        "date_posted": "July 27,  2025"
-    },
-    {
-        "author": "Lisle",
-        "title": "Blog post 2",
-        "content": "Second post content",
-        "date_posted": "July 28,  2025"
-    }
-]
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
