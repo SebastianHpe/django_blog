39ca4b6abce5611aa14c6e124453344e8d5dc037
SebastianHpe <sebastianhoppe@hotmail.de>
2025-07-30 16:42:24 +0200

    style: Apply black formatting

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,10 +3,9 @@ from .models import Post
 
 
 def home(request):
-    context = {
-        "posts": Post.objects.all()
-    }
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
+
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
