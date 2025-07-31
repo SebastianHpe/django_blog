b96f241a1106e958d7b0b6f78405e5a5ed16e88a
SebastianHpe <sebastianhoppe@hotmail.de>
2025-07-31 15:03:15 +0200

    feat: Add UserPostView

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -11,6 +12,16 @@ class PostListView(ListView):
     ordering = ["-date_posted"]
     paginate_by = 5
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+    
 
 class PostDetailView(DetailView):
     model = Post
