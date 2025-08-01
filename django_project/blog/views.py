0bf0bab1da85cd169e067fe8a151b63b03bcc945
SebastianHpe <sebastianhoppe@hotmail.de>
2025-08-01 07:49:27 +0200

    style: Apply black formatting

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 
 
@@ -12,6 +18,7 @@ class PostListView(ListView):
     ordering = ["-date_posted"]
     paginate_by = 5
 
+
 class UserPostListView(ListView):
     model = Post
     template_name = "blog/user_posts.html"
@@ -19,9 +26,9 @@ class UserPostListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
-    
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -35,6 +42,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ["title", "content"]
@@ -47,6 +55,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = "/"
@@ -54,6 +63,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
